e433344fab805d9223b8186cf9ac1ce4ee3207fc
nicolaas <nicolerrick@gmail.com>
2021-03-31 20:16:20 +0700

    first commit, example for string in js

---
+++ js/examples/string.js
@@ -0,0 +1,9 @@
+let username = "Nicolaas"; //this is the right way to use string
+
+let username2 = 'Nicolaas'; //single quote also can be used
+
+// let username 3 = 'Nicolaas";  //it is the wrong way to use string.
+
+let number = "23"; //although we type an number it still count as a string
+
+let random = "^*&%&^%^&$&$%^$%^" //typing an symbol also count as a string
