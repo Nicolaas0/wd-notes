ac4a99812b11cb0e8a4a1ef3c6b0be42c3260529
nicolaas <nicolerrick@gmail.com>
2021-03-31 21:28:57 +0700

    String basic (length,indices, and method)

--- js/examples/string.js
+++ js/examples/string.js
@@ -1,3 +1,5 @@
+//Creating an string
+
 let username = "Nicolaas"; //this is the right way to use string
 
 let username2 = 'Nicolaas'; //single quote also can be used
@@ -7,3 +9,32 @@ let username2 = 'Nicolaas'; //single quote also can be used
 let number = "23"; //although we type an number it still count as a string
 
 let random = "^*&%&^%^&$&$%^$%^" //typing an symbol also count as a string
+
+//String index
+
+let string = "Honey";
+
+string[0]; //the output will be "h" because its the first letter in the variable
+
+//String Length
+
+let string = "Chicken";
+
+string.length; //the output will be 7, because it will return how much letter in the variable
+
+// String method
+
+let string = "Nicolaas";
+//First method
+string.toLowerCase(); //make all letter to lower case
+
+//Second method
+string.toUpperCase(); //make all letter to uppper case
+
+//Third method
+string.trim(); //trim all the whitespace
+
+//We can also combine the method like
+string.trim().toUpperCase();
+
+
