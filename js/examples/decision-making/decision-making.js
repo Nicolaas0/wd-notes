db69d830d022c3ff725b26845ee6e93292759c89
Nicolaas0 <nicolerrick@gmail.com>
2021-04-11 20:08:49 +0700

    if and else if statements

--- js/examples/decision-making/decision-making.js
+++ js/examples/decision-making/decision-making.js
@@ -16,10 +16,16 @@ let strictEquality = "===";
 let strictNonEquality = "!==";
 
 //Conditionals
-
 let age = 10;
 
+//IF statement
 if (age >= 10){
     console.log('You are allowed to enter this zone.');
 }
 
+//ELSE IF statement
+if (age >= 10) {
+    console.log('You are still an adult');
+} else if(age <= 10) {
+    console.log('You are still a kid');
+}
