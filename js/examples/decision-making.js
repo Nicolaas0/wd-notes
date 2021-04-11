63867bf20631f0dfd1377a2d901cdecb7df582fc
Nicolaas0 <nicolerrick@gmail.com>
2021-04-11 18:48:03 +0700

    Comparison operator

---
+++ js/examples/decision-making.js
@@ -0,0 +1,19 @@
+//Comparison
+let greaterThan = ">";
+
+let lessThan = "<";
+
+let greaterThanOrEqualTo = ">=";
+
+let lessThanOrEqualTo = "<=";
+
+let equality = "==";
+
+let notEqual = "!=";
+
+let strictEquality = "===";
+
+let strictNonEquality = "!==";
+
+
+
