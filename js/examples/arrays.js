5cb3311228c507c61c1162080e099f86d6d55f58
Nicolaas0 <nicolerrick@gmail.com>
2021-04-13 20:48:02 +0700

    array method

--- js/examples/arrays.js
+++ js/examples/arrays.js
@@ -3,3 +3,18 @@ const empty = [];
 const clasmates = ['Nicolaas', 'Nadine', 'Nemo'];
 const oddNum = [1, 3, 5];
 const mixed = ['Nicolaas', 1, 'Nadine'];
+
+//Arrays methods:
+
+const arr = ['Hi'];
+
+//push()
+arr.push('One'); // output: ['Hi', 'One'] ;
+//pop()
+arr.pop(); //output: ['Hi];
+
+const arr2 = ['Hi', 'Ho'];
+//shift()
+arr.shift(); // output: ['Ho'];
+//unshift()
+arr.unshift('Ha'); // output: ['Ha', 'Ho'];
