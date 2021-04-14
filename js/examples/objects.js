ee980ffa44224b9981c7226d2564cdb429293928
Nicolaas0 <nicolerrick@gmail.com>
2021-04-14 19:00:13 +0700

    array+object

--- js/examples/objects.js
+++ js/examples/objects.js
@@ -18,3 +18,23 @@ myself.favMovie[1]; //output:'Friendzone'
 // add & changin properties in object
 myself.age = 18; //changin age value
 myself.favFodd = 'Fried Rice'; //add properties
+
+//ARRAY + OBJECTS
+
+const profile = [
+  {
+    username: "nicolaas0",
+    email: "nicolhh@gmail.com",
+    password: "huhu123",
+  },
+  {
+    username: "nadin123",
+    email: "cutegril123@gmail.com",
+    password: "bts223",
+  },
+  {
+    username: "npapap123",
+    email: "loremis@gmail.com",
+    password: "some12444",
+  },
+];
