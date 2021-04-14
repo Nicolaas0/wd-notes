3dc1c5be1a351c3ba97334ec5dbc075fba09de59
Nicolaas0 <nicolerrick@gmail.com>
2021-04-14 21:44:18 +0700

    function and arguments

---
+++ js/examples/functions.js
@@ -0,0 +1,13 @@
+//Defining our first function
+function first() {console.log('Hello World!');}
+//Executing the function
+first();
+
+//Arguments
+function greet(name) {console.log('Hi' + name);}
+//Executing the function
+greet('Nicolaas');
+//one more example
+function multiplication(a, b, c) { console.log(a * b * c); }
+//Executing the function
+multiplication(3, 4, 5);
