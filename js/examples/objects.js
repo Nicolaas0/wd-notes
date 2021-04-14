d8b1771e443e344aca5bfd9353decef4c0ecca6e
Nicolaas0 <nicolerrick@gmail.com>
2021-04-14 18:36:34 +0700

    accessing object and creating object

---
+++ js/examples/objects.js
@@ -0,0 +1,12 @@
+// Creating object
+const myself = {
+    firstName: 'Albert',
+    lastName: 'Nicolaas',
+    age: 12,
+    favMovie: ['Catch me if you can', 'Friendzone'],
+    happy:true
+}
+
+//accessing object
+myself.firstName; // output: 'Albert'
+myself.favMovie[1]; //output:'Friendzone'
